967a8299ff24202f43145f79ebab8e635c4275d5
Andrey Ivlev <warcraft1921@gmail.com>
2022-02-16 11:46:19 +0300

    architecture improved

--- homework1.py
+++ homework1.py
@@ -32,37 +32,50 @@ def calculate_async(coeffs, activation_funcs, x, y0, n):
     return y
 
 
-def main():
-    n = 100  # number of iterations
-    y0 = np.array([0.1, 0.1, 0.1])  # initial condition
+def show_result(y):
+    print(y)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot(y[:n - m, 0], y[:n - m, 1], y[:n - m, 2], color='blue', label='parametric curve')
+    ax.plot(y[n - m:, 0], y[n - m:, 1], y[n - m:, 2], color='pink', label='parametric curve')
+    ax.scatter(y[0, 0], y[0, 1], y[0, 2], s=[20], color='red')
 
+    plt.show()
+
+def stable_example():
+    y0 = np.array([0.1, 0.1, 0.1])  # initial condition
     # [X, 1, 2, 3], i - i'th neuron
     coeffs = np.array([[1, -0.5, 0.2, 0.7],
                       [1, 0.2, 0.1, 1.2],
                       [0, -0.3, -0.5, 0.5]])
-
-    # Various X(t) functions:
-    x1 = lambda t: 0
-    x2 = lambda t: 1./t
-    x3 = lambda t: np.sin((np.pi * t)/10)
-
     x = x1
-
-    # Various activation functions:
-    a_func1 = lambda h: np.tanh(h)
-    a_func2 = lambda h: np.exp(-h**2)
-
     activation_funcs = np.array([a_func1, a_func2, a_func1])
 
     y = calculate_sync(coeffs, activation_funcs, x, y0, n)
+    show_result(y)
 
-    print(y)
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot(y[:, 0], y[:, 1], y[:, 2], label='parametric curve')
-    ax.scatter(y0[0], y0[1], y0[2], s=[20], color='red')
+def cycle_example():
+    y0 = np.array([0.1, 0.1, 0.1])  # initial condition
+    # [X, 1, 2, 3], i - i'th neuron
+    coeffs = np.array([[1, -0.5, 0.2, 0.7],
+                      [1, 0.2, 0.9, 1.2],
+                      [0, -0.3, -0.5, 0.5]])
+    x = x1
+    activation_funcs = np.array([a_func1, a_func2, a_func1])
 
-    plt.show()
+    y = calculate_async(coeffs, activation_funcs, x, y0, n)
+
+    show_result(y)
 
 
-main()
+n = 1000  # number of iterations
+m = 10 # number of final iterations which are shown as pink
+# Various X(t) functions:
+x1 = lambda t: 0
+x2 = lambda t: 1./t
+x3 = lambda t: np.sin((np.pi * t)/10)
+# Various activation functions:
+a_func1 = lambda h: np.tanh(h)
+a_func2 = lambda h: np.exp(-h**2)
+
+cycle_example()
